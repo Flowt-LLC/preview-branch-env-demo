0edd08b1fac42b6883bb1a2b6e58baf751800dc8
scottw <ScottWeinstein@users.noreply.github.com>
2025-04-23 16:54:47 -0400

    Demo (#2)
    * more cleanup
    * demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
 export default function Home() {
   return (
-    <div className="grid">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h2 className="text-4xl font-bold text-center">DEMO_VAR</h2>
-        {process.env.DEMO_VAR ? (
-          <pre>
-            <code>{process.env.DEMO_VAR}</code>
-          </pre>
-        ) : (
-          "process.env.DEMO_VAR is undefined"
-        )}
-      </main>
-    </div>
+    <main className="flex flex-col">
+      <h2>DEMO_VAR</h2>
+      {process.env.DEMO_VAR ? (
+        <pre>
+          <code>{process.env.DEMO_VAR}</code>
+        </pre>
+      ) : (
+        "process.env.DEMO_VAR is undefined"
+      )}
+    </main>
   );
 }
---
+++ upsertEnv.tsx
@@ -0,0 +1,59 @@
+import { Vercel } from "@vercel/sdk";
+import assert from "assert";
+
+assert(process.env.VERCEL_ACCESS_TOKEN, "VERCEL_ACCESS_TOKEN is not set");
+assert(process.env.VERCEL_GIT_COMMIT_REF, "VERCEL_GIT_COMMIT_REF is not set");
+assert(process.env.VERCEL_GIT_COMMIT_REF !== "main", "must be a preview build");
+
+const vercel = new Vercel({
+  bearerToken: process.env.VERCEL_ACCESS_TOKEN,
+});
+const VERCEL_PROJECT_ID = "prj_MJY7gNdDbixlE8DWRZsDKKXHcJlQ";
+
+upsertEnvValue("DEMO_VAR").catch((err) => {
+  console.error("Failed to upsert env var", err);
+  process.exit(1);
+});
+
+async function upsertEnvValue(key: string) {
+  const addResponse = await vercel.projects.createProjectEnv({
+    idOrName: VERCEL_PROJECT_ID,
+    slug: "flowt",
+    teamId: "flowt",
+    upsert: "true",
+    requestBody: [
+      {
+        comment: "set via upsertEnvValue",
+        gitBranch: process.env.VERCEL_GIT_COMMIT_REF,
+        key,
+        value: `${key}_${process.env.VERCEL_GIT_COMMIT_REF}`,
+        target: ["preview"],
+        type: "encrypted",
+      },
+    ],
+  });
+  assert(
+    addResponse.failed.length == 0,
+    `Failed to set env item - ${JSON.stringify(addResponse)}`
+  );
+  console.log("Added environment variables: %o", addResponse);
+  const vercelEnvVal = await getVercelEnvItem(key);
+  assert(vercelEnvVal, `Failed to get envs - ${key} not found`);
+}
+
+async function getVercelEnvItem(key: string) {
+  const envData = await vercel.projects.filterProjectEnvs({
+    idOrName: VERCEL_PROJECT_ID,
+    slug: "flowt",
+    teamId: "flowt",
+    gitBranch: process.env.VERCEL_GIT_COMMIT_REF,
+  });
+  assert(
+    "envs" in envData,
+    `Failed to get envs - unexpected response ${JSON.stringify(envData)}`
+  );
+  return envData.envs.find(
+    (env) =>
+      env.key === key && env.gitBranch === process.env.VERCEL_GIT_COMMIT_REF
+  );
+}
